5e58ce79e6ae43bcdc127e878df3bdc28552d701
Vinh3 <101193855+vinh333@users.noreply.github.com>
2024-06-10 10:06:43 +0700

    init project

---
+++ src/hoidanit/Hello_world.java
@@ -0,0 +1,12 @@
+package hoidanit;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello_world");
+		System.out.println("Hello_world");
+		System.out.println("Hello_world");
+		System.out.println("Hello_world");
+
+	}
+}
